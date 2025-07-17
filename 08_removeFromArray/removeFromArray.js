49f1f7fdc4685a0b80f87a0d2e9eff45c3e4c45e
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-07-17 12:17:51 +0700

    Done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    for (let arg of args) {
+        for (i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] === arg) {
+                arr.splice(i, 1)
+            };
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
