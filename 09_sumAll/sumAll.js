3067340d6f6d00d963de11456610012adcc651b4
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-07-17 14:22:26 +0700

    Done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    if (typeof firstNum !== "number" || typeof lastNum !== "number" ||
+        firstNum % 1 !== 0 || lastNum % 1 !== 0 || firstNum <= 0 ||
+        lastNum <= 0) return "ERROR";
+    let total = 0;
+    if (firstNum < lastNum) {
+        for (let i = firstNum; i <= lastNum; i++) {
+            total += i;
+        }
+        return total;
+    } else {
+        for (let i = lastNum; i <= firstNum; i++) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
