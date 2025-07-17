f501a54b0677e8216c9015309fb850037881c77d
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-07-17 14:59:23 +0700

    Done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const result = (5 / 9 * (temp - 32)) % 1 === 0 ? (5 / 9 * (temp - 32)) :
+  parseFloat((5 / 9 * (temp - 32)).toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const result = ((9 / 5 * temp) + 32) % 1 === 0 ?  ((9 / 5 * temp) + 32) :
+  parseFloat(((9 / 5 * temp) + 32).toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
