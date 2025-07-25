cc5361c4a38ad553396628f96d95a2a867110ede
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-07-25 09:22:12 +0700

    Done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const polished = str.replaceAll(/[.,! ]/g, "").toLowerCase();
+    return polished === polished.split("").reverse().join("");
 };
 
 // Do not edit below this line
