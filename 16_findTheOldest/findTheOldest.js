15d7dfefca3b26750de157e3d8e91cd7bc7cc4a2
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-07-25 11:54:05 +0700

    Done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        const today = new Date();
+        if (!("yearOfDeath" in a)) a.yearOfDeath = today.getFullYear();
+        if (!("yearOfDeath" in b)) b.yearOfDeath = today.getFullYear();
+        const ageA = a.yearOfDeath - a.yearOfBirth;
+        const ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageB - ageA;
+    })
 
+    return people[0];
 };
 
 // Do not edit below this line
