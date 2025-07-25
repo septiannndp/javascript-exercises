9830bda5b430e230ae86a4b4bee11406551fe95e
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-07-25 11:30:46 +0700

    Done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count = typeof countArg === "number" ? countArg : parseInt(countArg);
+    if (count === 0) return 0;
+    if (count < 0) return "OOPS";
 
+    let secondPrev = 0;
+    let firstPrev = 1;
+
+    for (let i = 2; i <= count; i++) {
+        let curr = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = curr;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
